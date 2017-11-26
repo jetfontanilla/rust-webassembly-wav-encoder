ca26e7b8f3ae3b303326635ab344018fe8b332a0
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2017-11-26 10:41:39 -0500

    need known array buffer size at compile time

--- src/wav-encoder.rs
+++ src/wav-encoder.rs
@@ -2,12 +2,12 @@ fn main() {
     println!("encoder loaded");
 }
 
-fn merge_buffers(buffers: &[[f32]]) -> [f32] {
-    return buffers.flat_map(|buffer| buffer.iter()).collect();
+fn merge_buffers(buffers: &[[f32]], recording_length: i16) -> [f32] {
+    buffers.flat_map(|buffer| buffer.iter()).collect();
 }
 
 fn interleave(left_buffer: &[f32], right_buffer: &[f32]) -> [f32] {
-    return left_buffer.zip(right_buffer).collect();
+    left_buffer.zip(right_buffer).collect();
 }
 
 fn encode_wav(buffer: &[f32]) -> [u16] {
@@ -15,10 +15,10 @@ fn encode_wav(buffer: &[f32]) -> [u16] {
 }
 
 #[no_mangle]
-pub fn export_wav(left_buffers: &[[f32]], right_buffers: &[[f32]]) -> [u16] {
+pub fn export_wav(left_buffers: [[f32]], right_buffers: [[f32]], recording_length: i16) -> [u16] {
     let left_buffer = merge_buffers(&left_buffers);
     let right_buffer = merge_buffers(&right_buffers);
     let interleaved = interleave(&left_buffer, &right_buffer);
     
-    return encode_wav(&interleaved);
+    encode_wav(&interleaved);
 }

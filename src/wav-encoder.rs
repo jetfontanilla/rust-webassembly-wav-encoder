a3e5a9c0254763e671b9d1033aae9eac3a7cc2f4
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2017-12-14 06:49:02 -0500

    treat wasm inputs as unsafe pointers

--- src/wav-encoder.rs
+++ src/wav-encoder.rs
@@ -1,3 +1,5 @@
+use std::slice;
+
 fn main() {
     println!("encoder loaded");
 }
@@ -11,14 +13,20 @@ fn interleave(left_buffer: &[f32], right_buffer: &[f32]) -> [f32] {
 }
 
 fn encode_wav(buffer: &[f32]) -> [u16] {
-
+    [];
 }
 
 #[no_mangle]
-pub fn export_wav(left_buffers: [[f32]], right_buffers: [[f32]], recording_length: i16) -> [u16] {
-    let left_buffer = merge_buffers(&left_buffers);
-    let right_buffer = merge_buffers(&right_buffers);
+pub fn export_wav(left_buffers: *mut [[f32]],
+                  right_buffers: *mut [[f32]],
+                  recording_length: usize) -> [u16] {
+    unsafe {
+        let left_slice = slice::from_raw_parts(left_buffers, recording_length);
+        let right_slice = slice::from_raw_parts(right_buffers, recording_length);
+    }
+    let left_buffer = merge_buffers(&left_slice, recording_length);
+    let right_buffer = merge_buffers(&right_slice, recording_length);
     let interleaved = interleave(&left_buffer, &right_buffer);
-    
+
     encode_wav(&interleaved);
 }
